9b588cb14ecf5d9bb9bbd07fb88f295df14ccf6f
hojun3377 <hojun3377@gmail.com>
2024-05-15 23:07:17 +0900

    feat: jwt 발행 및 쿠키로 송신 구현
    - .env 파일 생성 및 코드 변경
    - jwt 토큰 생성 (src/controllers/loginController)
    - 쿠키 생성 및 송신 (src/controllers/loginController)
    
    ref: Github issues [#12]

--- packages/study-server/src/controllers/loginController.js
+++ packages/study-server/src/controllers/loginController.js
@@ -1,12 +1,10 @@
+import jwt from "jsonwebtoken";
+
 const login = (req, res) => {
   const { email, password } = req.body;
 
   /**
    * db 조회 로직 추가
-   */
-
-  /**
-   * jwt 토큰 발행 및 송신 추가
    */
 
   /**
@@ -17,6 +15,33 @@ const login = (req, res) => {
   if (email === "test" && password === "test") {
     // email, password 정보 있음
 
+    /**
+     * jwt 발행
+     * payload: { email }
+     * secret: process.env.JWT_SECRET_KEY
+     * expiresIn: 30m
+     * issuer: hojun
+     */
+    const token = jwt.sign({ email }, process.env.JWT_SECRET_KEY, {
+      expiresIn: "30m",
+      issuer: "hojun",
+    });
+
+    /**
+     * cookie 생성
+     * name: "token"
+     * value: jwt
+     * httpOnly: true
+     */
+    res.cookie("token", token, {
+      httpOnly: true,
+    });
+
+    /**
+     * response
+     * status: 200(OK)
+     * body: { message: `${email}님 로그인 성공` }
+     */
     res.status(200).json({ message: `${email}님 로그인 성공` });
   } else {
     // email, password 정보 없음
--- packages/study-server/src/index.js
+++ packages/study-server/src/index.js
@@ -1,14 +1,20 @@
 import express from "express";
+import dotenv from "dotenv";
 
 import loginController from "./controllers/loginController.js";
+
+/**
+ * .env파일에 정의된 환경변수 설정
+ */
+dotenv.config();
 
 /**
  * express 서버 생성 및 시작
  */
 const app = express();
 
-app.listen(1234, () => {
-  console.log(`Start server on port ${1234}!`);
+app.listen(process.env.PORT, () => {
+  console.log(`Start server on port ${process.env.PORT}!`);
 });
 
 /**
